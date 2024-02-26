5a86117864ec456ab03eeda3f37227cc1846c577
Ko <kochang.chu@gmail.com>
2024-02-26 17:49:14 +1100

    moved logic in controller

--- src/components/data/controller/stageDataController.tsx
+++ src/components/data/controller/stageDataController.tsx
@@ -17,6 +17,7 @@ export interface ICountdownData {
   until: Date;
   since: Date;
   wrapUps: IWrapUp[];
+  timeRemaining?: string;
 }
 
 interface IStageData {
@@ -70,9 +71,61 @@ export const WrapUpYellow: React.CSSProperties = {
   fontSize: "12em",
 }
 
+function calculateHours(distance: number) {
+  return Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+}
+
+function calculateSeconds(distance: number) {
+  return Math.floor((distance % (1000 * 60)) / 1000);
+}
+
+function calculateMinutes(distance: number) {
+  return Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+}
+
+function formatDigits(num: number) {
+  return num.toString().padStart(2, "0");
+}
+
+function compareDates(date1: Date, date2: Date) {
+  if (date1.getUTCFullYear() > date2.getUTCFullYear()) return 1;
+  if (date1.getUTCFullYear() < date2.getUTCFullYear()) return -1;
+  if (date1.getUTCMonth() > date2.getUTCMonth()) return 1;
+  if (date1.getUTCMonth() < date2.getUTCMonth()) return -1;
+  if (date1.getUTCDate() > date2.getUTCDate()) return 1;
+  if (date1.getUTCDate() < date2.getUTCDate()) return -1;
+  if (date1.getUTCHours() > date2.getUTCHours()) return 1;
+  if (date1.getUTCHours() < date2.getUTCHours()) return -1;
+  if (date1.getUTCMinutes() > date2.getUTCMinutes()) return 1;
+  if (date1.getUTCMinutes() < date2.getUTCMinutes()) return -1;
+  if (date1.getUTCSeconds() > date2.getUTCSeconds()) return 1;
+  if (date1.getUTCSeconds() < date2.getUTCSeconds()) return -1;
+
+  return 0;
+}
+
+export function calculateTimeDistance(sinceDate: Date, untilDate: Date) {
+  const now = GetTimeNow();
+
+  const comparison = compareDates(now, sinceDate);
+  const since = (comparison > 0 ? now : sinceDate).getTime();
+  const until = untilDate.getTime();
+  const diff = until - since;
+  return diff > 0 ? diff : 0;
+}
+
+export function calculateTimeRemaining(distance: number) {
+  const hours = calculateHours(distance);
+  const minutes = calculateMinutes(distance);
+  const seconds = calculateSeconds(distance);
+
+  return `${formatDigits(hours)}:${formatDigits(minutes)}:${formatDigits(
+    seconds
+  )}`;
+}
 export function GetStageListingData(): IStageData[] {
   // return a date 2 mins from now
-  const now = new Date(2024, 1, 24, 17, 55);
+  const now = new Date(2024, 1, 26, 17, 48);
   const twoMinutesLater = new Date(now);
   twoMinutesLater.setMinutes(now.getMinutes() + 2);
   const fiveMinutesLater = new Date(now);
--- src/components/ui/countdown.tsx
+++ src/components/ui/countdown.tsx
@@ -4,68 +4,18 @@ import {
   GetTimeNow,
   ICountdownData,
   IWrapUp,
+  calculateTimeDistance,
+  calculateTimeRemaining,
 } from "../data/controller/stageDataController";
 // write a count down component that takes a date prop and displays the time remaining until that date
 // the component should update every second
 // the component should display the time remaining in the format `hh:mm:ss`
 // if the date has passed, the component should display `00:00:00`
 
-function calculateHours(distance: number) {
-  return Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-}
-
-function calculateSeconds(distance: number) {
-  return Math.floor((distance % (1000 * 60)) / 1000);
-}
-
-function calculateMinutes(distance: number) {
-  return Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-}
-
-function formatDigits(num: number) {
-  return num.toString().padStart(2, "0");
-}
-
-function compareDates(date1: Date, date2: Date) {
-  if (date1.getUTCFullYear() > date2.getUTCFullYear()) return 1;
-  if (date1.getUTCFullYear() < date2.getUTCFullYear()) return -1;
-  if (date1.getUTCMonth() > date2.getUTCMonth()) return 1;
-  if (date1.getUTCMonth() < date2.getUTCMonth()) return -1;
-  if (date1.getUTCDate() > date2.getUTCDate()) return 1;
-  if (date1.getUTCDate() < date2.getUTCDate()) return -1;
-  if (date1.getUTCHours() > date2.getUTCHours()) return 1;
-  if (date1.getUTCHours() < date2.getUTCHours()) return -1;
-  if (date1.getUTCMinutes() > date2.getUTCMinutes()) return 1;
-  if (date1.getUTCMinutes() < date2.getUTCMinutes()) return -1;
-  if (date1.getUTCSeconds() > date2.getUTCSeconds()) return 1;
-  if (date1.getUTCSeconds() < date2.getUTCSeconds()) return -1;
-
-  return 0;
-}
-
-function calculateTimeDistance(sinceDate: Date, untilDate: Date) {
-  const now = GetTimeNow();
-
-  const comparison = compareDates(now, sinceDate);
-  const since = (comparison > 0 ? now : sinceDate).getTime();
-  const until = untilDate.getTime();
-  const diff = until - since;
-  return diff > 0 ? diff : 0;
-}
-
-function calculateTimeRemaining(distance: number) {
-  const hours = calculateHours(distance);
-  const minutes = calculateMinutes(distance);
-  const seconds = calculateSeconds(distance);
-
-  return `${formatDigits(hours)}:${formatDigits(minutes)}:${formatDigits(
-    seconds
-  )}`;
-}
-
 interface CountdownProps {
   data: ICountdownData;
 }
+
 
 export const Countdown: React.FC<CountdownProps> = (props) => {
   const [inSessionStyle, setInSessionStyle] = useState<React.CSSProperties>(
